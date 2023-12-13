61acf066b6747584846c0340fcf30d847ec37e30
0x-Stealth <42773083+0x-Stealth@users.noreply.github.com>
2023-12-13 18:02:24 +0000

    added conf (which doesn't work) and made bot work

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
-from modules import browser, cookies
+import discord
+from discord.ext import commands
+from discord import Intents
 
-def main():
-    browser.niggashit()
-    browser.aaah()
-    cookies.aaaaahhhh()
-    cookies.aaaahhh()
+intents = Intents.all() # can we go back to the days where there was no intents
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'i am {bot.user.name}')
+
+def send_content(content, embed=None):
+    channel = bot.get_channel(1)  # replace one with your channel id, cba making conf work yet
+    if embed:
+        bot.loop.create_task(channel.send(content, embed=embed))
+    else:
+        bot.loop.create_task(channel.send(content))
 
 if __name__ == "__main__":
-    main()
+    with open('token.txt', 'r') as file:
+        bot_token = file.read().strip()
+    bot.run(bot_token)
 
